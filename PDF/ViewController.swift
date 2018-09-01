b0d959742adf87f57adfa4bfbf61ce88ed3c99f0
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-01 18:03:34 -0500

    Build simple PDF viewer with functions for moving to the first page, last page and "printing".

--- PDF/ViewController.swift
+++ PDF/ViewController.swift
@@ -6,18 +6,57 @@
 //  Copyright © 2018 Charles Martin Reed. All rights reserved.
 //
 
+import PDFKit
 import UIKit
+
+@objcMembers //exposes your obj-c primitives to swift code. Compare to typing @objc before each method used by a selector.
 
 class ViewController: UIViewController {
 
+    //MARK:- PDF Properties
+    var pdfView: PDFView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //create and add pdf view
+        pdfView = PDFView()
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+        
+        //make the pdf take up the full screen
+        pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        //load our example pdf and make it load immediately
+        let url = Bundle.main.url(forResource: "employee-handbook", withExtension: "pdf")!
+        pdfView.document = PDFDocument(url: url)
+        
+        //double page spread layout
+        //pdfView.displayMode = .twoUpContinuous
+        //pdfView.displaysAsBook = true
+        
+        //MARK:- Navbar buttons
+        let printSelectionBtn = UIBarButtonItem(title: "Selection", style: .plain, target: self, action: #selector(printSelection))
+        let firstPageBtn = UIBarButtonItem(title: "First", style: .plain, target: self, action: #selector(firstPage))
+        let lastPageBtn = UIBarButtonItem(title: "Last", style: .plain, target: self, action: #selector(lastPage))
+        
+        navigationItem.rightBarButtonItems = [printSelectionBtn, firstPageBtn, lastPageBtn]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK:- Navbar button functions
+    func printSelection() {
+        print(pdfView.currentSelection ?? "No selection")
+    }
+    
+    func firstPage() {
+        pdfView.goToFirstPage(nil)
+    }
+    
+    func lastPage() {
+        pdfView.goToLastPage(nil)
     }
 
 
